f4aa07bc26a151bc7f7c241f7fdc5cad6f4ece21
Robert Gallegos <rlgallegos85@gmail.com>
2023-01-15 23:20:37 -0500

    First try at color block java code

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h3 = document.createElement('h3');
 h3.textContent = 'This content added by JavaScript';
 document.querySelector('body').appendChild(h3);
+
+const skyBlue = document.createElement('#sky-blue');
+
+skyBlue.addEventListener('click', function() {
+    body = document.createElement('body');
+    body.style.backgroundColor = '#87ceeb';
+})
