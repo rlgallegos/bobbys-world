22a904381df44d60677591ccab0ee1d3fa7b876d
Robert Gallegos <rlgallegos85@gmail.com>
2023-01-16 00:09:47 -0500

    Try at color block java code pt5

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const h3 = document.createElement('h3');
 h3.textContent = 'This content added by JavaScript';
 document.querySelector('body').appendChild(h3);
 
-const skyBlue = document.getElementById('#sky-blue');
+const skyBlue = document.getElementById('sky-blue');
 
 skyBlue.addEventListener('click', function() {
     const body = document.getElementById('body');
     body.style.backgroundColor = '#87ceeb';
 });
-
