18ca1b323d5ea9a9253034ca9efd2d04c3ba7a49
Robert Gallegos <rlgallegos85@gmail.com>
2023-01-16 00:55:11 -0500

    Try at color block java code pt9

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-const h3 = document.createElement('h3');
-h3.textContent = 'This content added by JavaScript';
-document.querySelector('body').appendChild(h3);
-
 const skyBlue = document.getElementById('sky-blue');
 
 skyBlue.addEventListener('click', function(e) {
