df8f83c3d5ea063d951d95f4f5abcf7182709984
Robert Gallegos <rlgallegos85@gmail.com>
2023-01-16 01:55:32 -0500

    trying to update cursor

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+
+//get variables for elements
 const skyBlue = document.getElementById('sky-blue');
 const forestGreen = document.getElementById('forest-green');
 const crimsonRed = document.getElementById('crimson-red');
 const white = document.getElementById('white');
-const text = document.getElementsByTagName('*');
 
+//set function to change pointer
 
+function makePointer(element) {
+    element.body.style.cursor = 'pointer'
+}
+
+//Sky Blue event liseteners
 skyBlue.addEventListener("click", function() {
     document.body.style.backgroundColor = '#87ceeb';
 });
+skyBlue.addEventListener('mouseover', makePointer);
 
 forestGreen.addEventListener("click", function() {
     document.body.style.backgroundColor = '#228B22';
@@ -15,9 +23,6 @@ forestGreen.addEventListener("click", function() {
 
 crimsonRed.addEventListener("click", function() {
     document.body.style.backgroundColor = '#900';
-    for (let i = 0; i < all.length; i++) {
-        all[i].color = 'white'
-    };
 });
 
 white.addEventListener("click", function() {
