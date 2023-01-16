7e09f81af967b1d88da5326bb91b9fef0c4aed88
Robert Gallegos <rlgallegos85@gmail.com>
2023-01-16 01:33:45 -0500

    finish color block java

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const skyBlue = document.getElementById('sky-blue');
+const forestGreen = document.getElementById('forest-green');
+const crimsonRed = document.getElementById('crimson-red');
+const white = document.getElementById('white');
+
 
 skyBlue.addEventListener("click", function() {
-    document.body.style.backgroundColor = 'white';
-    console.log("button clicked");
+    document.body.style.backgroundColor = '#87ceeb';
+});
+
+forestGreen.addEventListener("click", function() {
+    document.body.style.backgroundColor = '#228B22';
+});
+
+crimsonRed.addEventListener("click", function() {
+    document.body.style.backgroundColor = '#900';
+});
+
+white.addEventListener("click", function() {
+    document.body.style.backgroundColor = '#fff';
 });
