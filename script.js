d285330e55145154b0e2ba7cc81d655fbcf09251
Robert Gallegos <rlgallegos85@gmail.com>
2023-01-16 01:26:53 -0500

    Try at color block java code pt15

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const skyBlue = document.getElementById('sky-blue');
-const body = document.querySelector('body');
 
 skyBlue.addEventListener("click", function() {
     document.body.style.backgroundColor = 'white';
